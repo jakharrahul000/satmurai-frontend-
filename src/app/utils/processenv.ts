22281a07d6aac55bac3f9088a1b48b3cb6daf162
Rahul Jakhar <rahul.jakhar@undostres.com.mx>
2022-09-25 08:56:34 +0530

    changes port

--- src/app/utils/processenv.ts
+++ src/app/utils/processenv.ts
@@ -1,2 +1,2 @@
 export const baseUrl = process.env.REACT_APP_URL || 'http://localhost:4200';
-export const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3000/v1/';
+export const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3001/v1/';
